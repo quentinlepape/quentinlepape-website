2a58b3e0cb8e444093e8f549c37513094728d253
quentinlepape <quentinlepape@gmail.com>
2023-06-09 20:36:34 +0200

    feat: add `aria-labels` to `social-links` for accessibility

--- app/components/socialLinks/SocialLinks.tsx
+++ app/components/socialLinks/SocialLinks.tsx
@@ -6,7 +6,11 @@ export default function SocialLinks(): JSX.Element {
   return (
     <ul className={`${styles.socialLinksList} flex flex-row text-dark`}>
       <li className={styles.item}>
-        <Link href="mailto:quentinlepape@gmail.com" target="_blank">
+        <Link
+          href="mailto:quentinlepape@gmail.com"
+          target="_blank"
+          aria-label="Send me an email"
+        >
           <SocialIcons.Email
             width={"1.125rem"}
             height={"1.125rem"}
@@ -14,7 +18,11 @@ export default function SocialLinks(): JSX.Element {
         </Link>
       </li>
       <li className={styles.item}>
-        <Link href="https://www.linkedin.com/in/quentinlepape" target="_blank">
+        <Link
+          href="https://www.linkedin.com/in/quentinlepape"
+          target="_blank"
+          aria-label="Visit my Linkedin page"
+        >
           <SocialIcons.Linkedin
             width={"1.125rem"}
             height={"1.125rem"}
@@ -22,7 +30,11 @@ export default function SocialLinks(): JSX.Element {
         </Link>
       </li>
       <li className={styles.item}>
-        <Link href="https://www.instagram.com/quentinlepape/" target="_blank">
+        <Link
+          href="https://www.instagram.com/quentinlepape/"
+          target="_blank"
+          aria-label="Visit my Instagram page"
+        >
           <SocialIcons.Instagram
             width={"1.125rem"}
             height={"1.125rem"}
