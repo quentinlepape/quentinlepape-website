21995266803154a819254533ecb46f8d5d046ad0
quentinlepape <quentinlepape@gmail.com>
2023-06-09 15:16:44 +0200

    feat: responsive lightbox

--- app/components/lightbox/Lightbox.tsx
+++ app/components/lightbox/Lightbox.tsx
@@ -18,7 +18,7 @@ export default function Lightbox({
   return (
     <div
       className={`cursor-pointer fixed z-10 inset-0 h-full w-full bg-backdrop flex items-center justify-center ${
-        styles.lightbox
+        styles.lightboxWrapper
       } ${children ? "block opacity-100" : "hidden opacity-0"} transition`}
       onClick={onClose}
     >
@@ -26,17 +26,22 @@ export default function Lightbox({
         onClick={(event) => {
           event.stopPropagation();
         }}
-        className="w-4/5 max-w-video-720p relative cursor-default"
+        className={`${styles.lightbox} max-w-video-720p relative cursor-default`}
       >
         <div className="h-full w-full overflow-hidden border border-white/40 rounded-lg backdrop-blur bg-white/.06 text-white experimental-shadow">
-          <div className="flex flex-row justify-between border-b border-white/40">
+          <div
+            className={`${styles.containerTop} flex flex-row justify-between border-b border-white/40`}
+          >
             <div className="flex flex-row gap-3 items-center px-5 py-3">
-              {/* <Icon name="Rocket" size="16"></Icon> */}
               <div className="separate-2 separate-slash separate-opacity-50">
-                <div className="text-S text-bold text-uppercase text-white/80 inline opacity-50">
+                <div
+                  className={`${styles.subtitle} text-S text-bold text-uppercase text-white/80 inline opacity-50`}
+                >
                   {subtitle && subtitle}
                 </div>
-                <h3 className="text-S text-bold text-uppercase text-white/80 inline">
+                <h3
+                  className={`${styles.title} text-S text-bold text-uppercase text-white/80 inline`}
+                >
                   {title && title}
                 </h3>
               </div>
@@ -44,7 +49,7 @@ export default function Lightbox({
             {link && (
               <Link
                 href={link}
-                className="text-S text-white/80 border-l border-white/40 h-auto px-5 flex items-center justify-center shrink-0 hover:bg-white/.06 group relative left-0 hover:left-0.5 transition"
+                className={`${styles.link} text-S text-white/80 border-l border-white/40 h-auto px-5 flex items-center justify-center shrink-0 hover:bg-white/.06 group relative left-0 hover:left-0.5 transition`}
                 target="_blank"
               >
                 Learn more
@@ -68,6 +73,32 @@ export default function Lightbox({
             className={`${styles.wrapper} w-full overflow-hidden`}
           >
             {children}
+          </div>
+          <div
+            className={`${styles.containerBot} flex flex-row justify-between border-t border-white/40`}
+          >
+            <div className="flex flex-row gap-3 items-center px-5 py-3">
+              <div className="separate-2 separate-slash separate-opacity-50">
+                <h3 className="text-S text-bold text-uppercase text-white/80 inline">
+                  {"\u00A0"}
+                </h3>
+              </div>
+            </div>
+            {link && (
+              <Link
+                href={link}
+                className={`${styles.linkResponsive} text-S text-white/80 border-l border-white/40 h-auto px-5 flex items-center justify-center shrink-0 hover:bg-white/.06 group relative left-0 hover:left-0.5 transition`}
+                target="_blank"
+              >
+                Learn more
+                <Icon
+                  name="ArrowRight"
+                  size="14"
+                  flavor="thin"
+                  classNames="-mb-0.5 ml-2.5 transition"
+                />
+              </Link>
+            )}
           </div>
         </div>
         <button
