c992bf3570f254adfda5245d23280cf05d273c02
quentinlepape <quentinlepape@gmail.com>
2023-06-15 02:26:29 +0200

    progress(interactive globe): add locations to globe

--- app/components/globe/InteractiveGlobe.tsx
+++ app/components/globe/InteractiveGlobe.tsx
@@ -4,6 +4,10 @@ import dynamic from "next/dynamic";
 import { forwardRef, useEffect, useRef, useState } from "react";
 import { GlobeMethods } from "react-globe.gl";
 import * as THREE from "three";
+import {
+  CurriculumVitae,
+  ICurriculumVitaeLocationsWorked,
+} from "../../data/curriculumVitae";
 
 const GlobeTmpl = dynamic(() => import("./globeWrapper"), {
   ssr: false,
@@ -18,24 +22,31 @@ globeMaterial.bumpScale = 10;
 new THREE.TextureLoader().load(
   "//unpkg.com/three-globe/example/img/earth-water.png",
   (texture: any) => {
-    // globeMaterial.specularMap = texture;
+    // ! globeMaterial.specularMap = texture;
     globeMaterial.specular = new THREE.Color("grey");
-    globeMaterial.shininess = 15;
+    globeMaterial.shininess = 5;
   }
 );
 
 export default function InteractiveGlobe() {
   const globeRef = useRef<GlobeMethods>();
   const [isGlobeReady, setGlobeReady] = useState<boolean>();
+  const locations = CurriculumVitae.locationsWorked;
+
+  const getLat = (location: ICurriculumVitaeLocationsWorked) =>
+    location.geo.lat;
+  const getLon = (location: ICurriculumVitaeLocationsWorked) =>
+    location.geo.lon;
+  // ! const getAlt = (data: any) => data.elevation * 5e-5;
 
   useEffect(() => {
-    console.log(isGlobeReady);
     if (isGlobeReady) {
       // wait for scene to be populated (asynchronously)
       const directionalLight = globeRef
         .current!.scene()
         .children.find((obj3d) => obj3d.type === "DirectionalLight");
       directionalLight && directionalLight.position.set(1, 1, 1); // change light position to see the specularMap's effect
+      // ! globeRef.current!.scene().position.y = -100;
     }
   }, [isGlobeReady]);
 
@@ -43,7 +54,8 @@ export default function InteractiveGlobe() {
     <Globe
       ref={globeRef}
       width={260}
-      height={260}
+      height={260 * 2}
+      showGraticules
       // width={
       //   typeof window !== "undefined" && window.innerWidth > 575 ? 460 : 288
       // }
@@ -57,7 +69,11 @@ export default function InteractiveGlobe() {
       onGlobeReady={() => {
         setGlobeReady(true);
       }}
-      // atmosphereAltitude={0.15}
+      pointsData={locations}
+      pointLat={getLat}
+      pointLng={getLon}
+
+      // ! atmosphereAltitude={0.15}
     />
   );
 }
