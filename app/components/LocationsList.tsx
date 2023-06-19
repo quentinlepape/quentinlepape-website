0328aea3d2ac9176585e2b9e0e8c30381b1985bb
quentinlepape <quentinlepape@gmail.com>
2023-06-19 18:54:35 +0200

    refactor(LocationsList): simplify with `.map`

--- app/components/LocationsList.tsx
+++ app/components/LocationsList.tsx
@@ -1,32 +1,17 @@
-import {
-  CurriculumVitae,
-  ICurriculumVitaeLocationsWorked,
-} from "../data/curriculumVitae";
+import { CurriculumVitae } from "../data/curriculumVitae";
 import styles from "../layout.module.css";
-
-function Locations(locations: ICurriculumVitaeLocationsWorked[]) {
-  const locationsList: React.JSX.Element[] = [];
-  locations.forEach((location) => {
-    const secondaryLabel: React.JSX.Element | null = location.detail ? (
-      <span className="text-faded"> ({location.detail})</span>
-    ) : null;
-    locationsList.push(
-      <li className={styles.location}>
-        {location.locationName}
-        {secondaryLabel}
-      </li>
-    );
-  });
-
-  return locationsList;
-}
-
-// TODO: Update this code with exampke from https://nextjs.org/learn/foundations/from-javascript-to-react/displaying-data-with-props
 
 export default function LocationsList() {
   return (
     <ul className="text-medium text-S">
-      {Locations(CurriculumVitae.locationsWorked)}
+      {CurriculumVitae.locationsWorked.map((location, i) => (
+        <li className={styles.location} key={i}>
+          {location.locationName}
+          {location.detail && (
+            <span className="text-faded"> ({location.detail})</span>
+          )}
+        </li>
+      ))}
     </ul>
   );
 }
