a06a4070498a487cc1bf11242bda6fe19ec62ec8
quentinlepape <quentinlepape@gmail.com>
2023-06-19 19:47:08 +0200

    feature(LocationsList): live indicator on current location

--- app/components/LocationsList.tsx
+++ app/components/LocationsList.tsx
@@ -5,11 +5,12 @@ export default function LocationsList() {
   return (
     <ul className="text-medium text-S">
       {CurriculumVitae.locationsWorked.map((location, i) => (
-        <li className={styles.location} key={i}>
+        <li className={`${styles.location} flex flex-row items-center`} key={i}>
           {location.locationName}
           {location.detail && (
             <span className="text-faded"> ({location.detail})</span>
           )}
+          {location.current && <span className={styles.liveIndicator}></span>}
         </li>
       ))}
     </ul>
