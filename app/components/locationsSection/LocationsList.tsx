61027810a10f069444e981d00b7cd6baf7c123d8
quentinlepape <quentinlepape@gmail.com>
2023-06-21 00:13:02 +0200

    feat(LocationsSection): location item hover state

--- app/components/locationsSection/LocationsList.tsx
+++ app/components/locationsSection/LocationsList.tsx
@@ -7,30 +7,37 @@ import Icon from "../Icon";
 
 export default function LocationsList({
   onHover,
+  onHoverOff,
 }: {
   onHover: (focusedLocation: ICurriculumVitaeLocationsWorked) => void;
+  onHoverOff: (focusedLocation: undefined) => void;
 }) {
   return (
     <ul className="text-medium text-S">
       {CurriculumVitae.locationsWorked.map((location, i) => (
         <li
-          className={`${styles.location} flex flex-row items-center`}
+          className={`${styles.location} flex flex-row items-center justify-between`}
           onMouseEnter={(e) => {
             onHover(location);
           }}
+          onMouseLeave={(e) => {
+            onHoverOff(undefined);
+          }}
           key={i}
         >
+          <div className="flex flex-row items-center">
+            <span className={styles.pinIcon}>📍</span>
+            <span className={styles.locationName}>{location.locationName}</span>
+            {location.detail && (
+              <span className="text-faded"> ({location.detail})</span>
+            )}
+            {location.current && <span className={styles.liveIndicator}></span>}
+          </div>
           <div
             className={`${styles.hoverIndicator} flex flex-row items-center`}
           >
-            <Icon name="ArrowRight" flavor="thin" size="16" />
+            <Icon name="GlobeNetwork" flavor="thin" size="16" />
           </div>
-          <span className={styles.pinIcon}>📍</span>
-          {location.locationName}
-          {location.detail && (
-            <span className="text-faded"> ({location.detail})</span>
-          )}
-          {location.current && <span className={styles.liveIndicator}></span>}
         </li>
       ))}
     </ul>
